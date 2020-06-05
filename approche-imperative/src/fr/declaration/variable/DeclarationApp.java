57e92fc5700a10688baad02ee58d35eb97486c70
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-05 16:56:08 +0200

    Affichage de "randomString"

--- approche-imperative/src/fr/declaration/variable/DeclarationApp.java
+++ approche-imperative/src/fr/declaration/variable/DeclarationApp.java
@@ -3,6 +3,28 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
+		byte a = 1;
+		short b = 2;
+		int c = 3;
+		long d = 4L;
+		float f = 3.4F;
+		double e = 3.45;
+		char g = 'z';
+		boolean bool = true;
+		String s = "Amine";
+		
+		/*int i = 0; 
+		
+		for (i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("en dehors");
+		System.out.println(i);
+		*/
+		String randomString = "Voici le résultat d’un calcul:" +"\n" + " 1+5=6";
+		
+		System.out.println(randomString);
 		
 
 	}
