18b323225aa3aa0ce6aa21415082d9b69ded9476
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-08 15:00:25 +0200

    RechercheMin

--- approche-imperative/src/fr/algorithmie/RechercheMin.java
+++ approche-imperative/src/fr/algorithmie/RechercheMin.java
@@ -3,29 +3,26 @@ package algorithmie;
 /**
  * 
  * @author Mohamed Amine
- * Dans cette classe, on recherche le plus grand élément d'un tableau d'entiers. 
+ * Dans cette classe, on recherche le plus petit élément d'un tableau d'entiers. 
  *
  */
-
-public class RechercheMax {
+public class RechercheMin {
 
 	public static void main(String[] args) {
-		
 		
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
 		
 		int taille = array.length;
-		int max = array[0];
+		int min = array[0];
 		
 		for (int i = 0 ; i < taille ; i++)
 		{
-			if (array[i] > max)
+			if (array[i] < min)
 			{
-				max = array[i];
+				min = array[i];
 			}
 		}
-		System.out.println("Le plus grand élément de array est : " + max);
-		
+		System.out.println("Le plus petit élément de array est : " + min);
 
 	}
 
