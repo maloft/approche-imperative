9dbda711285ce8f84375819d2f1d3e5f5fce3459
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-06-08 12:18:18 +0200

    AffichageInverse : affichage du contenu du tableau array dans l'ordre inverse

--- approche-imperative/src/fr/algorithmie/AffichageInverse.java
+++ approche-imperative/src/fr/algorithmie/AffichageInverse.java
@@ -8,11 +8,22 @@ public class AffichageInverse {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
 		int taille = array.length;
 		
-		for (int i=0; i < taille; i++ )
+		//Affichage du contenu du tableau array
+		/* for (int i=0; i < taille; i++ )
 		{
 			System.out.println(array[i]);
 		}
-
+		*/
+		
+		
+		//Affichage du contenu de array dans l'ordre inverse
+		for (int i = taille - 1 ; i >= 0 ; i-- )
+		{
+			System.out.println(array[i]); 
+		
+		}
+		
+		
 	}
 
 }
